4f3f4fa56340eefd8ba9abe9ef769be7d23d13b2
Alvin Murimi <alvynirgz@gmail.com>
2023-10-26 21:14:48 +0300

    Add LICENSE.md

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pygsm7",
-    version="1.0.2",
+    version="1.0.3",
     description="GSM 7-bit Encoding and Decoding Module",
     author="Alvin Mayende",
     author_email="alvinmayende@gmail.com",
