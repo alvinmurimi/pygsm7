601236d9a6774c222080cc8b96cc6b491d740694
Alvin Murimi <alvynirgz@gmail.com>
2023-10-26 20:49:59 +0300

    Update README.md

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="pygsm7",
-    version="1.0.0",
+    version="1.0.2",
     description="GSM 7-bit Encoding and Decoding Module",
     author="Alvin Mayende",
     author_email="alvinmayende@gmail.com",
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
