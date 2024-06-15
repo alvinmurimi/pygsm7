468bc701a47e39af1e6fbf1ff8827c3d4239b003
Alvin Murimi <alvynirgz@gmail.com>
2024-06-15 20:09:29 +0300

    Fix setup.py to use README.md for package description

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="pygsm7",
     version="1.0.3",
     description="GSM 7-bit Encoding and Decoding Module",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author="Alvin Mayende",
     author_email="alvinmayende@gmail.com",
     python_requires='>=3.5',
